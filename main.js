a3cd9c139d74188f453bf5fab347cf73819f67a0
Root <sergeygrygorashenko@gmail.com>
2023-01-13 00:26:23 +0200

    add main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+// let item1 = 5;
+// let item2 = 10;
+// let result = item1 + item2
+
+// console.log(typeof result, result)
+
+// let name1 = "Hello"
+// let age = "33"
+// let age_num = +age
+// console.log(typeof name1, name1)
+// console.log(typeof age, age)
+// console.log(typeof age_num, age_num)
+
+let v1 = 33;
+let v2 = "Hello";
+let v3 = true;
+let v4 = false;
+let v5 = [33,44,55];
+let v6 = {"age": 33, "sal":2000, "name":"Elena"};
+let v7 = null;
+
+console.log(v1, typeof v1)
+console.log(v2, typeof v2)
+console.log(v3, typeof v3)
+console.log(v4, typeof v4)
+console.log(v5, typeof v5)
+console.log(v6, typeof v6)
+console.log(v7, typeof v7)
